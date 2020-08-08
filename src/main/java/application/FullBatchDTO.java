ea45dabf5a2d79c348da69903c485592f4fbbca0
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-08-08 19:34:42 +0300

    Add FullBatchDTO

---
+++ src/main/java/application/FullBatchDTO.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.time.LocalDateTime;
+
+public class FullBatchDTO {
+    private long id;
+    private String name;
+    private long year;
+    private String yeast;
+    private String fruit;
+    private String[] originCoordinates;
+    private String originDescription;
+    private LocalDateTime productionDate;
+    private String comment;
+}
