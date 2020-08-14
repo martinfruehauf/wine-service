873534ba5e406588de7670d177cddc32e202ecad
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-08-14 18:19:05 +0200

    Implement get batch by id in Resource

--- src/main/java/application/BatchResource.java
+++ src/main/java/application/BatchResource.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -40,5 +41,19 @@ public class BatchResource {
             listFullBatchDTO.add(new FullBatchDTO(batch));
         }
         return Response.ok().entity(listFullBatchDTO).build();  // create listFUllBatchDTO !!
+    }
+
+    @GET
+    @Path("/{batchId}")
+    public Response getBatchById(@PathParam("batchId") final long batchId) {
+        try {
+            LOG.info("Get Batch by id: {}", batchId);
+            Batch batch = batchService.getBatchById(batchId);
+            FullBatchDTO fullBatchDTO = new FullBatchDTO(batch);
+            return Response.ok().entity(fullBatchDTO).build();
+        } catch (IllegalArgumentException e) {
+            LOG.warn("Could not find batch by id: {}", batchId);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
