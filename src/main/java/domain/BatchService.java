85aa449bb8103bb16f39aa473436895cd1fd66ce
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-08-15 16:30:28 +0200

    Update BatchService to call Repository

--- src/main/java/domain/BatchService.java
+++ src/main/java/domain/BatchService.java
@@ -3,11 +3,14 @@ package domain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BatchService {
+    @Inject
+    private BatchRepository batchRepository;
 
     private static final Logger LOG = LoggerFactory.getLogger(BatchService.class);
 
@@ -41,16 +44,17 @@ public class BatchService {
     public List<Batch> getAllBatches() {
         LOG.info("Get all Batches");
 
-        return batchList;
+        return batchRepository.getBatches();
     }
 
     public Batch getBatchById(final long batchId) {
-        for (Batch batch : batchList) {
-            if (batch.getId() == batchId) {
-                return batch;
-            }
+        LOG.info("Get Batch by id: {}", batchId);
+        Batch batch = batchRepository.getBatchById(batchId);
+        if (batch == null) {
+            LOG.info("Batch by id {} does not exist", batchId);
+            throw new IllegalArgumentException("Could not find batch with id: " + batchId);
         }
-        throw new IllegalArgumentException("Could not find batch with id: " + batchId);
+        return batch;
     }
 
 }
