16bff086d781e11c4d6b5cead0e1d7a76818a15a
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-08-21 15:50:43 +0200

    Remove (unused) mocked list

--- src/main/java/domain/BatchService.java
+++ src/main/java/domain/BatchService.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BatchService {
@@ -17,25 +15,7 @@ public class BatchService {
     List<Batch> batchList; //Mocking the dataset
 
     public BatchService() {
-        batchList = new ArrayList<>();
-        batchList.add(new Batch(
-                1,
-                "MJ-Wein",
-                2020,
-                "Portwein",
-                "Apfel",
-                LocalDateTime.now(),
-                "Apefel waren ueberreif"));
-        batchList.add(new Batch(
-                2,
-                "MJ-Wein",
-                2020,
-                "Sherry",
-                "Apfel",
-                LocalDateTime.now(),
-                "Apefel waren ueberreif"));
     }
-
 
     public List<Batch> getAllBatches() {
         LOG.info("Get all Batches");
